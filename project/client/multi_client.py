9bb88947c4bc1f01f3baf8c136b75fb095e188b1
luke.munday <luke.munday@gmail.com>
2021-03-19 04:22:41 +0000

    Threaded the client to auto send messages to the server

--- project/client/multi_client.py
+++ project/client/multi_client.py
@@ -1,59 +1,63 @@
 import socket, select, string, sys
+import time
 
-#Helper function (formatting)
-def display() :
-	you="\33[33m\33[1m"+" You: "+"\33[0m"
-	sys.stdout.write(you)
-	sys.stdout.flush()
+class MultiClient:
+    def __init__(self, client_name):
 
-def main():
+        self.run_client = True
+        self.name = str(client_name)
 
-    if len(sys.argv)<2:
-        # host = input("Enter host ip address: ")
-        host = 'localhost'
-    else:
-        host = sys.argv[1]
+        if len(sys.argv)<2:
+            # host = input("Enter host ip address: ")
+            host = 'localhost'
+        else:
+            host = sys.argv[1]
 
-    port = 5001
-    
-    #asks for user name
-    name = input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
-    # name = 'client-name'
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-    
-    # connecting host
-    try :
-        s.connect((host, port))
-    except :
-        print ("\33[31m\33[1m Can't connect to the server \33[0m")
-        sys.exit()
-
-    #if connected
-    s.send(name.encode())
-    display()
-    while 1:
-        socket_list = [sys.stdin, s]
+        port = 5001
         
-        # Get the list of sockets which are readable
-        rList, wList, error_list = select.select(socket_list , [], [])
+        #asks for user name
+        # name = input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.settimeout(2)
         
-        for sock in rList:
-            #incoming message from server
-            if sock == s:
-                data = sock.recv(4096)
-                if not data :
-                    print ('\33[31m\33[1m \rDISCONNECTED!!\n \33[0m')
-                    sys.exit()
+        # connecting host
+        try :
+            self.s.connect((host, port))
+        except :
+            print ("\33[31m\33[1m Can't connect to the server \33[0m")
+            sys.exit()
+
+        #if connected
+        self.s.send(self.name.encode())
+        
+
+    def run(self):
+        print("name", self.name)
+        while self.run_client:
+            socket_list = [sys.stdin, self.s]
+            
+            # Get the list of sockets which are readable
+            rList, wList, error_list = select.select(socket_list , [], [])
+            
+            for sock in rList:
+                #incoming message from server
+                if sock == self.s:
+                    data = sock.recv(4096)
+                    if not data :
+                        print ('\33[31m\33[1m \rDISCONNECTED!!\n \33[0m')
+                        sys.exit()
+                    else :
+                        sys.stdout.write(data.decode())
+            
+                #user entered a message
                 else :
-                    sys.stdout.write(data.decode())
-                    display()
-        
-            #user entered a message
-            else :
-                msg=sys.stdin.readline()
-                s.send(msg.encode())
-                display()
+                    # msg=sys.stdin.readline()
+                    msg = "test message"
+                    self.s.send(msg.encode())
 
-if __name__ == "__main__":
-    main()
+            msg = "test message"
+            self.s.send(msg.encode())
+            time.sleep(.5)
+
+    def finish(self):
+        self.s.send("tata ".encode())
