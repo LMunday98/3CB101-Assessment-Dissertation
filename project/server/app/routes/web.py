27b36ebebdbec1e43f99e67c328be03d361a6b7c
luke.munday <luke.munday@gmail.com>
2021-03-23 09:23:36 +0000

    Seperated web and api routes

--- project/server/app/routes/web.py
+++ project/server/app/routes/web.py
@@ -2,24 +2,6 @@ import time
 from flask import Flask, Response, redirect, request, url_for, render_template, jsonify, stream_with_context
 from app import app
 
-from monitor import MyStreamMonitor
-
-stream = MyStreamMonitor()
-
-def get_data(stream):
-    time.sleep(.2)
-    data_stream = stream.get_data_string()
-    return data_stream
-
 @app.route('/')
 def root():
     return render_template('public/index.html')
-
-@app.route('/stream')
-def stream():
-    stream = MyStreamMonitor()
-    @stream_with_context
-    def eventStream():
-        while True:
-            yield 'data: %s\n\n' % (get_data(stream))
-    return Response(eventStream(), mimetype="text/event-stream")
