81bc92e44324099c0d5886c7991f6760c494ed16
luke.munday <luke.munday@gmail.com>
2021-03-23 04:03:31 +0000

    Calc fix

--- project/mpu/calcs.py
+++ project/mpu/calcs.py
@@ -5,9 +5,9 @@ class Calc:
         return math.sqrt((a*a)+(b*b))
 
     def get_y_rotation(self, x, y, z):
-        radians = math.atan2(x, Calc.dist(y,z))
+        radians = math.atan2(x, self.dist(y,z))
         return -math.degrees(radians)
 
     def get_x_rotation(self, x, y, z):
-        radians = math.atan2(y, Calc.dist(x,z))
+        radians = math.atan2(y, self.dist(x,z))
         return math.degrees(radians)
