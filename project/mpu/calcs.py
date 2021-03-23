b7335a188c49befdf7859af4f7e000b295429d45
luke.munday <luke.munday@gmail.com>
2021-03-23 03:51:10 +0000

    Fix calcs class

--- project/mpu/calcs.py
+++ project/mpu/calcs.py
@@ -1,13 +1,13 @@
 import math
 
 class Calc:
-    def dist(a,b):
+    def dist(self, a, b):
         return math.sqrt((a*a)+(b*b))
 
-    def get_y_rotation(x,y,z):
+    def get_y_rotation(self, x, y, z):
         radians = math.atan2(x, Calc.dist(y,z))
         return -math.degrees(radians)
 
-    def get_x_rotation(x,y,z):
+    def get_x_rotation(self, x, y, z):
         radians = math.atan2(y, Calc.dist(x,z))
         return math.degrees(radians)
