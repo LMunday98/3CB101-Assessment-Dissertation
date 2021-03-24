2ef8e62195c53f8d0d8d2aa41126d775453af7ad
luke.munday <luke.munday@gmail.com>
2021-03-24 20:12:52 +0000

    Client now handles reconnecting sensor and to host

--- project/client/multi_client.py
+++ project/client/multi_client.py
@@ -11,37 +11,66 @@ class MultiClient:
     def __init__(self, client_index):
         self.run_client = True
         self.name = str(client_index)
-        self.sensor = Sensor(client_index)
+        self.client_index = client_index
         self.port = 5001
 
         if len(sys.argv)<2:
             self.host = '192.168.0.184'
         else:
             self.host = sys.argv[1]
-            
+
+    def setup_sensor(self):
+        while True:
+            try:
+                self.sensor = Sensor(self.client_index)
+                print ("\33[32m\33[1mSuccessfully setup sensor \33[0m")
+                break
+            except Exception as e:
+                print ("\33[31m\33[1mError setting up the sensor \33[0m")
+                time.sleep(1)
+
     def establish_connection(self):
-        # connecting host
-        try :
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.s.settimeout(2)
-            self.s.connect((self.host, self.port))
-            self.s.send(self.name.encode())
-            print ("\33[32m\33[1mSuccessfully connected to the server \33[0m")
-        except :
-            print ("\33[31m\33[1mCan't connect to the server \33[0m")
+        while True:
+            try :
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.s.settimeout(2)
+                self.s.connect((self.host, self.port))
+                self.s.send(self.name.encode())
+                print ("\33[32m\33[1mSuccessfully connected to the server \33[0m")
+                break
+            except :
+                print ("\33[31m\33[1mCan't connect to the server \33[0m")
+                time.sleep(1)
+
+    def read_sensor(self):
+        try:
+            sensor_data = self.sensor.get_data()
+            data_string = pickle.dumps(sensor_data)
+            return data_string
+        except:
+            print ("\n\33[93m\33[1mReinitalising sensor \33[0m")
+            self.setup_sensor()
+
+    def send_data(self, data_to_send):
+        try:
+            self.s.send(data_to_send)
+        except:
+            print ("\n\33[93m\33[1mReconnecting to server \33[0m")
+            self.establish_connection()
+
 
     def run(self):
+        self.setup_sensor()
+        self.establish_connection()
+        time.sleep(.2)
+
         while self.run_client:
             try:
-                sensor_data = self.sensor.get_data()
-                data_string = pickle.dumps(sensor_data)
-                self.s.send(data_string)
+                data_to_send = self.read_sensor()
+                self.send_data(data_to_send)
                 time.sleep(.2)
             except Exception as e:
                 print (e)
-                print ("\n\33[93m\33[1mReconnecting to server \33[0m")
-                self.establish_connection()
-                time.sleep(1)
 
     def finish(self):
         try:
