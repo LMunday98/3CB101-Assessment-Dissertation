81d87462786f5239b9e69099ad9747c69d2c7158
luke.munday <luke.munday@gmail.com>
2021-03-26 02:35:30 +0000

    Refactor socket calls into function call

--- project/server/app/static/assets/js/socket_calls.js
+++ project/server/app/static/assets/js/socket_calls.js
@@ -1,27 +1,13 @@
-$(function() {
-    $('a#calibrate').bind('click', function() {
-      $.getJSON('/socket_call?socket_code=cal',
-        function(data) {
-      });
-      return false;
-    });
-  });
+button_bind('#calibrate', 'cal')
+button_bind('#session_start', 'session_start')
+button_bind('#session_end', 'session_end')
 
-$(function() {
-$('a#session_start').bind('click', function() {
-    $.getJSON('/socket_call?socket_code=session_start',
-    function(data) {
-        console.log("start");
+function button_bind(button_id, socket_code) {
+    $(function() {
+        $(button_id).bind('click', function() {
+            let call_url = '/socket_call?socket_code=' + socket_code;
+            $.getJSON(call_url, function(data) {} );
+            return false;
+        });
     });
-    return false;
-});
-});
-
-$(function() {
-$('a#session_end').bind('click', function() {
-    $.getJSON('/socket_call?socket_code=session_end',
-    function(data) {
-    });
-    return false;
-});
-});
+}
