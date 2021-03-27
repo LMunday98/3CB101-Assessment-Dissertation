348c76034719efabd10008e75c6f82bf210d2eba
luke.munday <luke.munday@gmail.com>
2021-03-27 18:01:44 +0000

    Removed constructor from data handler

--- project/web_server/server/data_handler.py
+++ project/web_server/server/data_handler.py
@@ -1,10 +1,6 @@
 import pickle
 
 class DataHandler:
-
-    def __init__(self):
-        x=1
-
     def record_data(self, sent_data):
         decoded_data = pickle.loads(sent_data)
         rower_index = decoded_data.get_rowerId()
