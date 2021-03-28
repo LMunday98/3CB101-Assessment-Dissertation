c032eeff8c9c40b75227006826b98efa522a2622
luke.munday <luke.munday@gmail.com>
2021-03-28 21:32:48 +0100

    Website correctly reads rower's info and data dict

--- project/web_server/server/data_handler.py
+++ project/web_server/server/data_handler.py
@@ -4,24 +4,26 @@ class DataHandler:
     def __init__(self):
         self.rower_dicts = []
         self.rower_indexes = [0, 1, 2, 3]
-        self.seats = ['stroke', 'stroke2', 'bow2', 'bow']
-        self.measurements = ['gx', 'gy', 'gz', 'sax', 'say', 'saz', 'rx', 'ry']
+
+        self.seat_labels = ['stroke', 'stroke2', 'bow2', 'bow']
+        self.info_labels = ['rower_index', 'seat', 'datetime']
+        self.data_labels = ['gx', 'gy', 'gz', 'ax', 'ay', 'az', 'sgx', 'sgy', 'sgz', 'sax', 'say', 'saz', 'rx', 'ry']
+
         self.setup_data_dicts()
 
     def setup_data_dicts(self):
         for rower_index in self.rower_indexes:
-            rower_dict = {}
-            # Add rower id
-            rower_dict['rower_index'] = rower_index
-            # Add seat
-            rower_dict['seat'] = self.seats[rower_index]
-            # Add measurements
-            for measurement in self.measurements:
-                rower_dict[measurement] = 0
-            # Addd timestamp
-            rower_dict['timestamp'] = ''
+            info_dict = {}
+            data_dict = {}
+            for label in self.info_labels:
+                info_dict[label] = ''
+            for label in self.data_labels:
+                data_dict[label] = 0
+            rower_dict = {
+                'info' : info_dict,
+                'data' : data_dict
+            }
             self.rower_dicts.append(rower_dict)
-        print(self.rower_dicts)
 
     def record_data(self, sent_data):
         decoded_data = pickle.loads(sent_data)
